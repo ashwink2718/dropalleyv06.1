19379f019cb073d641bd2d1984ab8bb0dfb1de64
ashwink2718 <ashwink2718@gmail.com>
2018-09-07 20:23:50 +0200

    Add create and destroy to sessions controller

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,10 @@ Rails.application.routes.draw do
   get 'brands', to: 'site#brands'
   get 'thanks', to: 'site#thanks'
   get 'orderconfirm', to: 'site#orderconfirm'
+  
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
 
   resources :users
   resources :orders
